77afdbc28fd1031a1ff3a4cc81920ec66ad2d9c7
quaran <skrsoma37@gmail.com>
2025-12-17 01:11:12 +0900

    feat: add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import "@mantine/core/styles.css";
 import "@mantine/charts/styles.css";
-import "@mantine/modals/styles.css";
 import { MantineProvider } from "@mantine/core";
 import { theme } from "./theme";
+import "./index.css";
 
 export default function App() {
     return <MantineProvider theme={theme}>App</MantineProvider>;
