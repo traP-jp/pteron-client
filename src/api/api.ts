ad23bec67b13f9a34564752a04c9ef4f50bf2af8
uni-kakurenbo <kohma1116@gmail.com>
2025-12-17 16:24:56 +0900

    fix

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,7 +3,7 @@ import createClient from "openapi-fetch";
 import type { paths as internalApiPath } from "./schema/internal";
 import type { paths as publicApiPath } from "./schema/public";
 
-export const ApiClient = {
+export const api = {
     internal: createClient<internalApiPath>({ baseUrl: "api/internal" }),
     public: createClient<publicApiPath>({ baseUrl: "api/v1" }),
 };
