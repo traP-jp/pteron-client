c107138ae66b4676e58aa8281c577ede9a4af965
mikannkann <82451989+mikannkann@users.noreply.github.com>
2025-12-17 15:56:07 +0900

    Revert "refactor: run format"

--- src/pages/UserProfile.tsx
+++ src/pages/UserProfile.tsx
@@ -1,12 +1,12 @@
 import { useParams } from "react-router-dom";
 
 export const UserProfile = () => {
-    const { userId } = useParams();
+  const { userId } = useParams();
 
-    return (
-        <div>
-            <h2>User Profile</h2>
-            <p>User ID: {userId}</p>
-        </div>
-    );
+  return (
+    <div>
+      <h2>User Profile</h2>
+      <p>User ID: {userId}</p>
+    </div>
+  );
 };
