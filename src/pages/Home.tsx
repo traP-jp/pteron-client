5c37cdb8119e73f8cb5d89709da79b4197561b6d
o_o <otukado@gmail.com>
2025-12-18 13:38:00 +0900

    feat: 順位変動の矢印を追加 (#40)
    * feat: 順位変動の矢印を追加
    
    * refactor: TrendIndicator の中身をいろいろ共通化
    
    ---------
    
    Co-authored-by: uni-kakurenbo <kohma1116@gmail.com>

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,10 +2,15 @@ import { PAvater } from "/@/components/PAvatar";
 import { toBranded } from "/@/types/entity";
 import type { ProjectName, UserName } from "/@/types/entity";
 
+import { TrendIndicator } from "../components/TrendIndicator";
+
 export const Home = () => {
     return (
         <div>
             <h1>Home</h1>
+            <TrendIndicator diff={0} />
+            <TrendIndicator diff={100} />
+            <TrendIndicator diff={-100} />
 
             <PAvater
                 type="user"
