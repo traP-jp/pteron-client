0062dc03f7ca397283b857db301285ec614692f8
rurun <docile.notepad.0n@icloud.com>
2025-12-19 14:05:50 +0900

    add checkout(wip)

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,9 +1,44 @@
-const Checkout = () => {
-    return (
-        <div>
-            <h1>Checkout</h1>
-        </div>
-    );
-};
+import { Button, Container, Flex, Grid } from "@mantine/core";
 
-export default Checkout;
+export function Checkout() {
+    return (
+        <Container>
+            <Flex
+                gap="xl"
+                justify="center"
+                align="center"
+                direction="column"
+                wrap="wrap"
+            >
+                <Grid
+                // justify="center"
+                // align="center"
+                >
+                    <Grid.Col span={3}>
+                        <Flex
+                            gap="xl"
+                            justify="center"
+                            align="center"
+                            direction="column"
+                            wrap="wrap"
+                        ></Flex>
+                    </Grid.Col>
+                    <Grid.Col
+                        span={3}
+                        h={120}
+                    >
+                        2
+                    </Grid.Col>
+                    <Grid.Col
+                        span={3}
+                        h={100}
+                    >
+                        3
+                    </Grid.Col>
+                </Grid>
+                <Button>Button 2</Button>
+                <Button>Button 3</Button>
+            </Flex>
+        </Container>
+    );
+}
