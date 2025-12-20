8aeaac565760233381e708e0edab3ea28503308a
mikannkann <82451989+mikannkann@users.noreply.github.com>
2025-12-20 17:56:00 +0900

    refactor: Use a shared `ApiConfig` object for API client initialization.

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,13 +1,15 @@
-import { Api as InternalApiClient } from "./schema/internal";
+import { type ApiConfig, Api as InternalApiClient } from "./schema/internal";
 import { Api as PublicApiClient } from "./schema/public";
 import { Api as TraqApiClient } from "./schema/traq";
 
 export * from "./paths";
 
+const config: ApiConfig = {};
+
 const apis = {
-    internal: new InternalApiClient({ baseURL: "/api/internal" }),
-    public: new PublicApiClient({ baseURL: "/api/v1" }),
-    traq: new TraqApiClient({}),
+    internal: new InternalApiClient(config),
+    public: new PublicApiClient(config),
+    traq: new TraqApiClient(config),
 };
 
 export default apis;
